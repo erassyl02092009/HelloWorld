fa4023a68a0b98ba43a310611f9ca71b97236662
erassyl02092009 <119171041+erassyl02092009@users.noreply.github.com>
2023-03-12 19:01:33 +0600

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello world");
+    }
+
+    void Update()
+    {
+
+    }
+}
+
